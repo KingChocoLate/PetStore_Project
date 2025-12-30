e3cdf07ae0f432875ba63cb5aec649b889e1a2c3
SOPHAT <freedomearning123@gmail.com>
2025-12-30 17:35:55 +0700

    admin dashboard and orders partially done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div class="nav_display h-[120px]">
-    <Navigation />
-  </div>
   <RouterView/>
-  
 </template>
 
 <script lang="ts">
-  import Navigation from './components/navigation.vue'
   export default {
     name: 'App',
-    components: {
-      Navigation
-    }
   }
 </script>
